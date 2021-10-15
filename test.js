92493cd6c0a1ce97fb44a722a3afdf9a2a7997fc
ektmd7676 <90033731+ektmd7676@users.noreply.github.com>
2021-10-15 13:23:54 +0900

    Add files via upload
    d

---
+++ test.js
@@ -0,0 +1,8 @@
+function check(form){
+    if(form.userid.value == "qqq" && form.userpassword.value == "qqq"){
+        window.open("dksl.html");
+    }
+    else{
+        alert("error");
+    }
+}
